565a426b8f3fc1c0c410e496f9382409dfc893d7
Phil Wang <lucidrains@gmail.com>
2023-09-23 18:15:01 +0200

    0.0.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'speculative-decoding',
   packages = find_packages(exclude=[]),
-  version = '0.0.9',
+  version = '0.0.10',
   license='MIT',
   description = 'Speculative Decoding',
   author = 'Phil Wang',
